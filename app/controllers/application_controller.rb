214815246aaf2c577bc323aaf460a384332dbd8b
TSephanya Brun <tsbrun@icloud.com>
2020-12-24 15:40:22 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
     session[:cart] ||= [] 
   end
 
-  helper_method :cart
+  helper_method :cart # need to explicitly set helper method to give view access
+  # not enough to do so just in the tests (see products_index_spec, line 10)
 end
